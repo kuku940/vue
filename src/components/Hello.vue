88fe7d7994a81114c94aa62922393d500ec7e3f3
Roin Zhang <roin.zhang@skyscanner.net>
2019-07-22 14:16:13 +0800

    Initial commit

---
+++ src/components/Hello.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Hello',
+    data () {
+      return {
+        msg: 'Hello Vue.js!'
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
